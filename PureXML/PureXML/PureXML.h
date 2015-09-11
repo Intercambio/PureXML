e54d788870d3220a89097995c20aedce25f91fd4
Tobias Kräntzer <info@tobias-kraentzer.de>
2015-09-11 18:36:41 +0200

    Initial Commit

---
+++ PureXML/PureXML/PureXML.h
@@ -0,0 +1,19 @@
+//
+//  PureXML.h
+//  PureXML
+//
+//  Created by Tobias Kräntzer on 11.09.15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PureXMLiOS.
+FOUNDATION_EXPORT double PureXMLVersionNumber;
+
+//! Project version string for PureXMLiOS.
+FOUNDATION_EXPORT const unsigned char PureXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PureXMLiOS/PublicHeader.h>
+
+
