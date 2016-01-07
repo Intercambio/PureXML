aeb3f8cd4d06169e4e1c14337a31abe96a52ff08
Tobias Kräntzer <info@tobias-kraentzer.de>
2016-01-07 19:52:21 +0100

    Add class for qualified names

---
+++ PureXML/PureXML/PXQName.m
@@ -0,0 +1,47 @@
+//
+//  PXQName.m
+//  PureXML
+//
+//  Created by Tobias Kräntzer on 07.01.16.
+//  Copyright © 2016 Tobias Kräntzer. All rights reserved.
+//
+
+#import "PXQName.h"
+
+@implementation PXQName
+
+#pragma mark Life-cycle
+
+- (instancetype)initWithName:(NSString *)name namespace:(NSString *)namespace
+{
+    self = [super init];
+    if (self) {
+        _name = [name copy];
+        _namespace = [namespace copy];
+    }
+    return self;
+}
+
+#pragma mark NSObject
+
+- (BOOL)isEqual:(PXQName *)object
+{
+    if (![self.name isEqualToString:object.name]) {
+        return NO;
+    }
+    
+    if (![self.namespace isEqualToString:object.namespace]) {
+        return NO;
+    }
+    
+    return YES;
+}
+
+#pragma mark NSCopying
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return [[PXQName alloc] initWithName:self.name namespace:self.namespace];
+}
+
+@end
--- PureXML/PureXML/PureXML.h
+++ PureXML/PureXML/PureXML.h
@@ -16,6 +16,7 @@ FOUNDATION_EXPORT const unsigned char PureXMLVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PureXML/PublicHeader.h>
 
+#import <PureXML/PXQName.h>
 #import <PureXML/PXDocument.h>
 #import <PureXML/PXNode.h>
 #import <PureXML/PXElement.h>
