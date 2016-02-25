02fb2c2bdc045b3b6936a94a049e02896a9cf293
Tobias Kräntzer <info@tobias-kraentzer.de>
2016-02-25 09:16:12 +0100

    Make the PXDocumentValueTransformer public

--- PureXML/PureXML/PureXML.h
+++ PureXML/PureXML/PureXML.h
@@ -23,3 +23,4 @@ FOUNDATION_EXPORT const unsigned char PureXMLVersionString[];
 #import <PureXML/PXText.h>
 #import <PureXML/PXInputStream.h>
 #import <PureXML/PXOutputStream.h>
+#import <PureXML/PXDocumentValueTransformer.h>
